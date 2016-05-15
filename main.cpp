ce5de30e1ee264f11f94b777e8bef6953907c957
mkaminaga <mkaminaga2.718@gmail.com>
2016-05-15 11:28:31 +0900

    TEXT -> T

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "tchar.h"
 #include "stdio.h"
 
-LPTSTR tchar2cw(TCHAR c, LPTSTR cw);
+LPTSTR tchar2cw(TCHAR c, LPTSTR* cw);
 
 int _tmain(int argc, TCHAR* argv[]) {
 
 	/* Error message */
 	if (argc <= 1) {
-		MessageBox(NULL, TEXT("No arguments"), TEXT("error"), MB_OK);
+		MessageBox(NULL, _T("No arguments"), _T("error"), MB_OK);
 		return -1;
 	}
 
@@ -17,66 +17,66 @@ int _tmain(int argc, TCHAR* argv[]) {
 	int length = (int) ::_tcslen(argv[1]); //size_t -> int
 	LPTSTR cw = 0;
 	for (i = 0; i < length; i++) {
-		tchar2cw(argv[1][i], cw);
+		tchar2cw(argv[1][i], &cw);
 		_tprintf(_T("%c: %s\n"), argv[1][i], cw);
 	}
 
 	return 0;
 }
 
-LPTSTR tchar2cw(TCHAR c, LPTSTR cw) {
+LPTSTR tchar2cw(TCHAR c, LPTSTR* cw) {
 	switch(c) {
 		/* Alphabets */
-		case 'A': cw = TEXT("・－"); break;
-		case 'B': cw = TEXT("－・・・"); break;
-		case 'C': cw = TEXT("－・－・"); break;
-		case 'D': cw = TEXT("－・・"); break;
-		case 'E': cw = TEXT("・"); break;
-		case 'F': cw = TEXT("・・－・"); break;
-		case 'G': cw = TEXT("－－・"); break;
-		case 'H': cw = TEXT("・・・・"); break;
-		case 'I': cw = TEXT("・・"); break;
-		case 'J': cw = TEXT("・－－－"); break;
-		case 'K': cw = TEXT("－・－"); break;
-		case 'L': cw = TEXT("・－・・"); break;
-		case 'M': cw = TEXT("－－"); break;
-		case 'N': cw = TEXT("－・"); break;
-		case 'O': cw = TEXT("－－－"); break;
-		case 'P': cw = TEXT("・－－・"); break;
-		case 'Q': cw = TEXT("－－・－"); break;
-		case 'R': cw = TEXT("・－・"); break;
-		case 'S': cw = TEXT("・・・"); break;
-		case 'T': cw = TEXT("－"); break;
-		case 'U': cw = TEXT("・・－"); break;
-		case 'V': cw = TEXT("・・・－"); break;
-		case 'W': cw = TEXT("・－－"); break;
-		case 'X': cw = TEXT("－・・－"); break;
-		case 'Y': cw = TEXT("－・－－"); break;
-		case 'Z': cw = TEXT("－－・・"); break;
+		case 'A': *cw = _T("・－"); break;
+		case 'B': *cw = _T("－・・・"); break;
+		case 'C': *cw = _T("－・－・"); break;
+		case 'D': *cw = _T("－・・"); break;
+		case 'E': *cw = _T("・"); break;
+		case 'F': *cw = _T("・・－・"); break;
+		case 'G': *cw = _T("－－・"); break;
+		case 'H': *cw = _T("・・・・"); break;
+		case 'I': *cw = _T("・・"); break;
+		case 'J': *cw = _T("・－－－"); break;
+		case 'K': *cw = _T("－・－"); break;
+		case 'L': *cw = _T("・－・・"); break;
+		case 'M': *cw = _T("－－"); break;
+		case 'N': *cw = _T("－・"); break;
+		case 'O': *cw = _T("－－－"); break;
+		case 'P': *cw = _T("・－－・"); break;
+		case 'Q': *cw = _T("－－・－"); break;
+		case 'R': *cw = _T("・－・"); break;
+		case 'S': *cw = _T("・・・"); break;
+		case 'T': *cw = _T("－"); break;
+		case 'U': *cw = _T("・・－"); break;
+		case 'V': *cw = _T("・・・－"); break;
+		case 'W': *cw = _T("・－－"); break;
+		case 'X': *cw = _T("－・・－"); break;
+		case 'Y': *cw = _T("－・－－"); break;
+		case 'Z': *cw = _T("－－・・"); break;
 
 		/* Numbers */
-		case '1': cw = TEXT("・－－－－"); break;
-		case '2': cw = TEXT("・・－－－"); break;
-		case '3': cw = TEXT("・・・－－"); break;
-		case '4': cw = TEXT("・・・・－"); break;
-		case '5': cw = TEXT("・・・・・"); break;
-		case '6': cw = TEXT("－・・・・"); break;
-		case '7': cw = TEXT("－－・・・"); break;
-		case '8': cw = TEXT("－－－・・"); break;
-		case '9': cw = TEXT("－－－－・"); break;
-		case '0': cw = TEXT("－－－－－"); break;
+		case '1': *cw = _T("・－－－－"); break;
+		case '2': *cw = _T("・・－－－"); break;
+		case '3': *cw = _T("・・・－－"); break;
+		case '4': *cw = _T("・・・・－"); break;
+		case '5': *cw = _T("・・・・・"); break;
+		case '6': *cw = _T("－・・・・"); break;
+		case '7': *cw = _T("－－・・・"); break;
+		case '8': *cw = _T("－－－・・"); break;
+		case '9': *cw = _T("－－－－・"); break;
+		case '0': *cw = _T("－－－－－"); break;
 
 		/* Simbols */
-		case '.': cw = TEXT("・－・－・－");
-		case ',': cw = TEXT("－－・・－－");
-		case '?': cw = TEXT("・・－－・・");
-		case '-': cw = TEXT("－・・・・－");
-		case '/': cw = TEXT("－・・－・");
-		case '@': cw = TEXT("・－－・－・");
-		case ' ': cw = TEXT("　　　");
+		case '.': *cw = _T("・－・－・－");
+		case ',': *cw = _T("－－・・－－");
+		case '?': *cw = _T("・・－－・・");
+		case '-': *cw = _T("－・・・・－");
+		case '/': *cw = _T("－・・－・");
+		case '@': *cw = _T("・－－・－・");
+		case ' ': *cw = _T("　　　");
 
 		/* Nothing */
 		default: break;
 	}
-	return cw;
+	return *cw;
 }
