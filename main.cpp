7aa0d67d0b8dc5cdd063f357854c8b0c86725a63
mkaminaga <mkaminaga2.718@gmail.com>
2016-05-19 13:51:33 +0900

    Output() became more smarter

--- main.cpp
+++ main.cpp
@@ -43,53 +43,56 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 void OutputMorse(TCHAR c) {
 	Morse& morse = Morse::GetInstance();
+	int code  =0;
+
 	switch (c) {
-		case _T('a'): case _T('A'): morse.OutputTerminalStringAndSound(CW_CODE_A, c); break;
-		case _T('b'): case _T('B'): morse.OutputTerminalStringAndSound(CW_CODE_B, c); break;
-		case _T('c'): case _T('C'): morse.OutputTerminalStringAndSound(CW_CODE_C, c); break;
-		case _T('d'): case _T('D'): morse.OutputTerminalStringAndSound(CW_CODE_D, c); break;
-		case _T('e'): case _T('E'): morse.OutputTerminalStringAndSound(CW_CODE_E, c); break;
-		case _T('f'): case _T('F'): morse.OutputTerminalStringAndSound(CW_CODE_F, c); break;
-		case _T('g'): case _T('G'): morse.OutputTerminalStringAndSound(CW_CODE_G, c); break;
-		case _T('h'): case _T('H'): morse.OutputTerminalStringAndSound(CW_CODE_H, c); break;
-		case _T('i'): case _T('I'): morse.OutputTerminalStringAndSound(CW_CODE_I, c); break;
-		case _T('j'): case _T('J'): morse.OutputTerminalStringAndSound(CW_CODE_J, c); break;
-		case _T('k'): case _T('K'): morse.OutputTerminalStringAndSound(CW_CODE_K, c); break;
-		case _T('l'): case _T('L'): morse.OutputTerminalStringAndSound(CW_CODE_L, c); break;
-		case _T('m'): case _T('M'): morse.OutputTerminalStringAndSound(CW_CODE_M, c); break;
-		case _T('n'): case _T('N'): morse.OutputTerminalStringAndSound(CW_CODE_N, c); break;
-		case _T('o'): case _T('O'): morse.OutputTerminalStringAndSound(CW_CODE_O, c); break;
-		case _T('p'): case _T('P'): morse.OutputTerminalStringAndSound(CW_CODE_P, c); break;
-		case _T('q'): case _T('Q'): morse.OutputTerminalStringAndSound(CW_CODE_Q, c); break;
-		case _T('r'): case _T('R'): morse.OutputTerminalStringAndSound(CW_CODE_R, c); break;
-		case _T('s'): case _T('S'): morse.OutputTerminalStringAndSound(CW_CODE_S, c); break;
-		case _T('t'): case _T('T'): morse.OutputTerminalStringAndSound(CW_CODE_T, c); break;
-		case _T('u'): case _T('U'): morse.OutputTerminalStringAndSound(CW_CODE_U, c); break;
-		case _T('v'): case _T('V'): morse.OutputTerminalStringAndSound(CW_CODE_V, c); break;
-		case _T('w'): case _T('W'): morse.OutputTerminalStringAndSound(CW_CODE_W, c); break;
-		case _T('x'): case _T('X'): morse.OutputTerminalStringAndSound(CW_CODE_X, c); break;
-		case _T('y'): case _T('Y'): morse.OutputTerminalStringAndSound(CW_CODE_Y, c); break;
-		case _T('z'): case _T('Z'): morse.OutputTerminalStringAndSound(CW_CODE_Z, c); break;
+		case _T('a'): case _T('A'): code = CW_CODE_A; break;
+		case _T('b'): case _T('B'): code = CW_CODE_B; break;
+		case _T('c'): case _T('C'): code = CW_CODE_C; break;
+		case _T('d'): case _T('D'): code = CW_CODE_D; break;
+		case _T('e'): case _T('E'): code = CW_CODE_E; break;
+		case _T('f'): case _T('F'): code = CW_CODE_F; break;
+		case _T('g'): case _T('G'): code = CW_CODE_G; break;
+		case _T('h'): case _T('H'): code = CW_CODE_H; break;
+		case _T('i'): case _T('I'): code = CW_CODE_I; break;
+		case _T('j'): case _T('J'): code = CW_CODE_J; break;
+		case _T('k'): case _T('K'): code = CW_CODE_K; break;
+		case _T('l'): case _T('L'): code = CW_CODE_L; break;
+		case _T('m'): case _T('M'): code = CW_CODE_M; break;
+		case _T('n'): case _T('N'): code = CW_CODE_N; break;
+		case _T('o'): case _T('O'): code = CW_CODE_O; break;
+		case _T('p'): case _T('P'): code = CW_CODE_P; break;
+		case _T('q'): case _T('Q'): code = CW_CODE_Q; break;
+		case _T('r'): case _T('R'): code = CW_CODE_R; break;
+		case _T('s'): case _T('S'): code = CW_CODE_S; break;
+		case _T('t'): case _T('T'): code = CW_CODE_T; break;
+		case _T('u'): case _T('U'): code = CW_CODE_U; break;
+		case _T('v'): case _T('V'): code = CW_CODE_V; break;
+		case _T('w'): case _T('W'): code = CW_CODE_W; break;
+		case _T('x'): case _T('X'): code = CW_CODE_X; break;
+		case _T('y'): case _T('Y'): code = CW_CODE_Y; break;
+		case _T('z'): case _T('Z'): code = CW_CODE_Z; break;
 
-		case _T('1'): morse.OutputTerminalStringAndSound(CW_CODE_1, c); break;
-		case _T('2'): morse.OutputTerminalStringAndSound(CW_CODE_2, c); break;
-		case _T('3'): morse.OutputTerminalStringAndSound(CW_CODE_3, c); break;
-		case _T('4'): morse.OutputTerminalStringAndSound(CW_CODE_4, c); break;
-		case _T('5'): morse.OutputTerminalStringAndSound(CW_CODE_5, c); break;
-		case _T('6'): morse.OutputTerminalStringAndSound(CW_CODE_6, c); break;
-		case _T('7'): morse.OutputTerminalStringAndSound(CW_CODE_7, c); break;
-		case _T('8'): morse.OutputTerminalStringAndSound(CW_CODE_8, c); break;
-		case _T('9'): morse.OutputTerminalStringAndSound(CW_CODE_9, c); break;
-		case _T('0'): morse.OutputTerminalStringAndSound(CW_CODE_0, c); break;
+		case _T('1'): code = CW_CODE_1; break;
+		case _T('2'): code = CW_CODE_2; break;
+		case _T('3'): code = CW_CODE_3; break;
+		case _T('4'): code = CW_CODE_4; break;
+		case _T('5'): code = CW_CODE_5; break;
+		case _T('6'): code = CW_CODE_6; break;
+		case _T('7'): code = CW_CODE_7; break;
+		case _T('8'): code = CW_CODE_8; break;
+		case _T('9'): code = CW_CODE_9; break;
+		case _T('0'): code = CW_CODE_0; break;
 
-		case _T('.'): morse.OutputTerminalStringAndSound(CW_CODE_PER, c); break;
-		case _T(','): morse.OutputTerminalStringAndSound(CW_CODE_COM, c); break;
-		case _T('?'): morse.OutputTerminalStringAndSound(CW_CODE_QUE, c); break;
-		case _T('-'): morse.OutputTerminalStringAndSound(CW_CODE_HIF, c); break;
-		case _T('/'): morse.OutputTerminalStringAndSound(CW_CODE_SLA, c); break;
-		case _T('@'): morse.OutputTerminalStringAndSound(CW_CODE_ATM, c); break;
-		case _T(' '): morse.OutputTerminalStringAndSound(CW_CODE_SPACE, c); break;
+		case _T('.'): code = CW_CODE_PER; break;
+		case _T(','): code = CW_CODE_COM; break;
+		case _T('?'): code = CW_CODE_QUE; break;
+		case _T('-'): code = CW_CODE_HIF; break;
+		case _T('/'): code = CW_CODE_SLA; break;
+		case _T('@'): code = CW_CODE_ATM; break;
+		case _T(' '): code = CW_CODE_SPACE; break;
 
-		default: morse.OutputTerminalStringAndSound(CW_CODE_UNKNOWN, c); break;
+		default: code = CW_CODE_UNKNOWN; break;
 	}
+	morse.OutputTerminalStringAndSound(code, c);
 }
